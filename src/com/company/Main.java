592a8ddc092fee137cf7da2b46c9c538ffe78838
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-25 12:00:36 -0800

    test data and start parsing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,33 @@
 package com.company;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class Main {
+
+    public static void main(String[] args) {
+        final String boardGames = """
+                {
+                    "keys": [
+                        "title",
+                        "genre",
+                        "price",
+                        "max_players",
+                        "published",
+                        "quantity"
+                    ],
+                    "values": [
+                        ["Irish Gauge", "strategy", 3499, 4, true, 20],
+                        ["Black Orchestra", "co-op", 4499, 5, true, 5],
+                        ["Three Times the Clever!", "dice", 2499, 99, false, 0]
+                    ]
+                }
+                """;
+
+        JSONObject jsonObject = new JSONObject(boardGames);
+        JSONArray keys = jsonObject.getJSONArray("keys");
+
+        
+
+    }
 }
